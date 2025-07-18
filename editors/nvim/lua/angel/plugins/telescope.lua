7cfb1b1edcd76ae947dc6577bf8ad7e6c40de528
Angel Szymczak <angel.commits@proton.me>
2025-07-18 14:07:48 -0300

    refact: improve telescope config

--- editors/nvim/lua/angel/plugins/telescope.lua
+++ editors/nvim/lua/angel/plugins/telescope.lua
@@ -27,6 +27,33 @@ return {
     telescope.setup({
       defaults = {
         path_display = { "smart" },
+
+        file_ignore_patterns = {
+          "node_modules",
+          ".git/objects/",
+          "dist/",
+          "build/",
+          "%.jpg",
+          "%.jpeg",
+          "%.png",
+          "%.gif",
+          "%.webp",
+        },
+
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            preview_width = 0.55,
+            results_width = 0.8,
+          },
+          vertical = {
+            mirror = false,
+          },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
