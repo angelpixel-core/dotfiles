9ce56530171e321dfba88244274230ca2107e01e
Angel DevStack <me@angel-solutions.studio>
2025-11-06 15:26:04 -0300

    feat: remove chatgpt, improve alpha landing

--- editors/nvim/lua/angel/plugins/alpha.lua
+++ editors/nvim/lua/angel/plugins/alpha.lua
@@ -1,36 +1,40 @@
 return {
   "goolord/alpha-nvim",
   event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
 
-    -- Set header
+    -- Logo
     dashboard.section.header.val = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
+      "    █████╗ ███╗   ██╗ ██████╗ ███████╗██╗        ",
+      "   ██╔══██╗████╗  ██║██╔════╝ ██╔════╝██║        ",
+      "   ███████║██╔██╗ ██║██║  ███╗█████╗  ██║        ",
+      "   ██╔══██║██║╚██╗██║██║   ██║██╔══╝  ██║        ",
+      "   ██║  ██║██║ ╚████║╚██████╔╝███████╗███████╗   ",
+      "   ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚══════╝   ",
+      "                                                 ",
+      "                🄳 🄴 🅅  🅂 🅃 🄰 🄲 🄺                 ",
     }
 
-    -- Set menu
+    -- Botones del dashboard
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-      dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+      dashboard.button("e", "  Nuevo archivo", "<cmd>ene<CR>"),
+      dashboard.button("SPC ee", "  > Toggle Explorador de Archivos", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("SPC ff", "󰱼  Buscar archivo", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPC fs", "  Buscar palabra", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("SPC wr", "󰁯  Restaurar sesión", "<cmd>SessionRestore<CR>"),
+      dashboard.button("q", "  Salir", "<cmd>qa<CR>"),
     }
 
-    -- Send config to alpha
+    dashboard.section.footer.val = function()
+      return "⚡ Neovim configurado por Angel | https://github.com/angel-devstack"
+    end
+
+    dashboard.opts.opts.noautocmd = true
     alpha.setup(dashboard.opts)
 
-    -- Disable folding on alpha buffer
     vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
   end,
 }
